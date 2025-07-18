7090f599b353695ab00fe148e78214fed3ab85cd
btscolombia <83001322+btscolombia@users.noreply.github.com>
2025-07-18 08:51:09 -0500

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,22 +20,22 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', $_ENV['WORDPRESS_DB_NAME'] ?: 'railway');
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define('DB_USER', $_ENV['WORDPRESS_DB_USER'] ?: 'root');
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define('DB_PASSWORD', $_ENV['WORDPRESS_DB_PASSWORD']);
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', $_ENV['WORDPRESS_DB_HOST'] ?: 'mysql.railway.internal');
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8mb4');
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
@@ -48,14 +48,10 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         $_ENV['WORDPRESS_AUTH_KEY'] ?: 'tu-clave-aqui');
+define('SECURE_AUTH_KEY',  $_ENV['WORDPRESS_SECURE_AUTH_KEY'] ?: 'tu-clave-aqui');
+define('LOGGED_IN_KEY',    $_ENV['WORDPRESS_LOGGED_IN_KEY'] ?: 'tu-clave-aqui');
+define('NONCE_KEY',        $_ENV['WORDPRESS_NONCE_KEY'] ?: 'tu-clave-aqui');
 
 /**#@-*/
 
@@ -71,32 +67,24 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
  *
  * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
-$table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
- */
-define( 'WP_DEBUG', false );
-
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+// Configuración SSL para Cloudflare
+if (isset($_SERVER['HTTP_CF_VISITOR']) && strpos($_SERVER['HTTP_CF_VISITOR'], 'https')) {
+    $_SERVER['HTTPS'] = 'on';
 }
 
-/** Sets up WordPress vars and included files. */
+// URLs dinámicas
+define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
+
+// Configuración adicional
+define('WP_DEBUG', $_ENV['WORDPRESS_DEBUG'] === 'true');
+define('AUTOMATIC_UPDATER_DISABLED', true);
+
+$table_prefix = $_ENV['WORDPRESS_TABLE_PREFIX'] ?: 'wp_';
+
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+}
+
 require_once ABSPATH . 'wp-settings.php';
+?> 
